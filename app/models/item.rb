93086e320657f9123669157a8dd33c310050aa66
Avi Flombaum <4515+aviflombaum@users.noreply.github.com>
2024-05-09 12:58:39 -0400

    Hook to send push on new feed item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :feed
+
+  after_create :send_notification
+
+  private
+
+  def send_notification
+    PushSubscription.all.each { |p| p.send_notification(title: title) }
+  end
 end
